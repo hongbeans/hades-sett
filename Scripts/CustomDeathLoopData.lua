c6a38dd30b73cc00a0b5eb2db2077cc874ac5c91
Carmen Cheung <carmenc4@uci.edu>
2024-12-16 15:11:20 -0800

    Removing Meg romance scene and Than rejection choice

--- Scripts/CustomDeathLoopData.lua
+++ Scripts/CustomDeathLoopData.lua
@@ -1,9 +1,45 @@
--- Remove Megaera bedroom scene
--- ModUtil.Table.NilMerge( DeathLoopData, {
--- 	DeathAreaBedroom = {
--- 		StartUnthreadedEvents = {
--- 			[5] = true,
--- 			[6] = true,
--- 		}
--- 	}
--- })
+-- Remove Megaera bedroom scene and Thanatos rejection
+ModUtil.Table.NilMerge( DeathLoopData, {
+	DeathAreaBedroom = {
+		StartUnthreadedEvents = {
+            -- Megaera scenes
+			[5] = true,
+			[6] = true,
+            -- Thanatos scenes
+            [7] = {
+                Args = {
+                    DistanceTrigger = {
+                        Args = {
+                            TextLineSet = {
+                                BecameCloseWithThanatos01 = {
+                                    [3] = {
+                                        Choices = {
+                                            [2] = true,
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            [8] = {
+                Args = {
+                    DistanceTrigger = {
+                        Args = {
+                            TextLineSet = {
+                                BecameCloseWithThanatos01_B = {
+                                    [3] = {
+                                        Choices = {
+                                            [2] = true,
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+		}
+	}
+})
