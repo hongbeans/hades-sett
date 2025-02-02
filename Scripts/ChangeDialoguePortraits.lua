df172ab9f27b252327bcaf903bb2460587c9ee07
hongbeans <infernalsong@gmail.com>
2025-02-02 12:07:03 -0800

    Change Aphelios portraits for certain dialogue lines

---
+++ Scripts/ChangeDialoguePortraits.lua
@@ -0,0 +1,100 @@
+-- NPCData changes
+ModUtil.Table.Merge( UnitSetData.NPCs, {
+    NPC_Thanatos_01 = {
+        InteractTextLineSets = {
+            ThanatosBackstory02 = {
+                [3] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosAboutDeath01 = {
+                [4] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosAboutErisAspect01 = {
+                [3] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosAboutAchilles01 = {
+                [4] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosAboutNyxAndDusa03 = {
+                [4] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosAboutRelationship01 = {
+                [3] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosAboutRelationship02 = {
+                [2] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosAboutRelationship03 = {
+                [5] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosWithNyx02 = {
+                [1] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosFieldAboutLegendary01 = {
+                [3] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            }
+        },
+        RepeatableTextLineSets = {
+            -- Hold for dialogue before Thanatos_0441
+            ThanatosHomeMaxChat03 = {
+                [1] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosHomeMaxChat02 = {
+                [1] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosHomeIntermissionChat02 = {
+                [2] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosHomeIntermissionChat01 = {
+                [2] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            }
+        },
+        GiftTextLineSets = {
+            ThanatosGift07_A = {
+                [4] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosGift09 = {
+                [4] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                },
+                [6] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            },
+            ThanatosGift10 = {
+                [6] = {
+                    Portrait = "Portrait_Thanatos_Shy_01"
+                }
+            }
+        }
+    }
+})
