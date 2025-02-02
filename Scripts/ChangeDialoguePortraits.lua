7e5954fceaa7c6fe08b4d624179d2bd250cdbf72
hongbeans <infernalsong@gmail.com>
2025-02-02 14:55:50 -0800

    Fixing crash on smooch-able load

--- Scripts/ChangeDialoguePortraits.lua
+++ Scripts/ChangeDialoguePortraits.lua
@@ -1,104 +1,107 @@
 -- NPCData changes
-ModUtil.Table.Merge( UnitSetData.NPCs, {
-    NPC_Thanatos_01 = {
-        InteractTextLineSets = {
-            ThanatosBackstory02 = {
-                [3] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosAboutDeath01 = {
-                [4] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosAboutErisAspect01 = {
-                [3] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosAboutAchilles01 = {
-                [4] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosAboutNyxAndDusa03 = {
-                [4] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosAboutRelationship01 = {
-                [3] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosAboutRelationship02 = {
-                [2] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosAboutRelationship03 = {
-                [5] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosWithNyx02 = {
-                [1] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosFieldAboutLegendary01 = {
-                [3] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            }
-        },
-        RepeatableTextLineSets = {
-            ThanatosHomeChat29 = {
-                [1] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosHomeMaxChat03 = {
-                [1] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosHomeMaxChat02 = {
-                [1] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosHomeIntermissionChat02 = {
-                [2] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosHomeIntermissionChat01 = {
-                [2] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            }
-        },
-        GiftTextLineSets = {
-            ThanatosGift07_A = {
-                [4] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
-                }
-            },
-            ThanatosGift09 = {
-                [4] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
+local function addShyPortraits()
+    ModUtil.Table.Merge( UnitSetData.NPCs, {
+        NPC_Thanatos_01 = {
+            InteractTextLineSets = {
+                ThanatosBackstory02 = {
+                    [3] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
                 },
-                [6] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
+                ThanatosAboutDeath01 = {
+                    [4] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosAboutErisAspect01 = {
+                    [3] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosAboutAchilles01 = {
+                    [4] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosAboutNyxAndDusa03 = {
+                    [4] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosAboutRelationship01 = {
+                    [3] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosAboutRelationship02 = {
+                    [2] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosAboutRelationship03 = {
+                    [5] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosWithNyx02 = {
+                    [1] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosFieldAboutLegendary01 = {
+                    [3] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
                 }
             },
-            ThanatosGift10 = {
-                [6] = {
-                    Portrait = "Portrait_Thanatos_Shy_01"
+            RepeatableTextLineSets = {
+                ThanatosHomeChat29 = {
+                    [1] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosHomeMaxChat03 = {
+                    [1] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosHomeMaxChat02 = {
+                    [1] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosHomeIntermissionChat02 = {
+                    [2] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosHomeIntermissionChat01 = {
+                    [2] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                }
+            },
+            GiftTextLineSets = {
+                ThanatosGift07_A = {
+                    [4] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosGift09 = {
+                    [4] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    },
+                    [6] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
+                },
+                ThanatosGift10 = {
+                    [6] = {
+                        Portrait = "Portrait_Thanatos_Shy_01"
+                    }
                 }
             }
         }
-    }
-})
+    })
+end
+OnAnyLoad{ addShyPortraits }
