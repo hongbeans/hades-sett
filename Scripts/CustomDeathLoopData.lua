a115d5d3c395edfe80dfc1c29084b2f41dbcab7c
hongbeans <infernalsong@gmail.com>
2025-04-09 20:57:19 -0700

    Fixed bedroom intermission not triggering

--- Scripts/CustomDeathLoopData.lua
+++ Scripts/CustomDeathLoopData.lua
@@ -1,62 +1,72 @@
 -- Remove Megaera bedroom scene and Thanatos rejection
-ModUtil.Table.NilMerge( DeathLoopData, {
-	DeathAreaBedroom = {
-		StartUnthreadedEvents = {
-            -- Megaera scenes
-			[5] = true,
-			[6] = true,
-            -- Thanatos scenes
-            [7] = {
-                Args = {
-                    DistanceTrigger = {
-                        Args = {
-                            TextLineSet = {
-                                BecameCloseWithThanatos01 = {
-                                    [3] = {
-                                        Choices = {
-                                            [2] = true,
+ModUtil.LoadOnce(function()
+    ModUtil.Table.NilMerge( DeathLoopData, {
+        DeathAreaBedroom = {
+            StartUnthreadedEvents = {
+                -- Megaera scenes
+                [5] = {
+                    FunctionName = true
+                },
+                [6] = {
+                    FunctionName = true
+                },
+                -- Thanatos scenes
+                [7] = {
+                    Args = {
+                        DistanceTrigger = {
+                            Args = {
+                                TextLineSet = {
+                                    BecameCloseWithThanatos01 = {
+                                        [3] = {
+                                            Choices = {
+                                                [2] = true,
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
-                }
-            },
-            [8] = {
-                Args = {
-                    DistanceTrigger = {
-                        Args = {
-                            TextLineSet = {
-                                BecameCloseWithThanatos01_B = {
-                                    [3] = {
-                                        Choices = {
-                                            [2] = true,
+                },
+                [8] = {
+                    Args = {
+                        DistanceTrigger = {
+                            Args = {
+                                TextLineSet = {
+                                    BecameCloseWithThanatos01_B = {
+                                        [3] = {
+                                            Choices = {
+                                                [2] = true,
+                                            }
                                         }
                                     }
                                 }
                             }
                         }
                     }
-                }
-            },
-            -- Meg + Than scenes
-            [9] = true,
-            [10] = true,
-		}
-	}
-})
-
--- Remove Megaera smooch statuses
-ModUtil.Table.NilMerge( UnitSetData.NPCs, {
-    NPC_FurySister_01 = {
-        RepeatableTextLineSets = {
-            MegIntermissionChat01 = true,
-            MegIntermissionChat02 = true,
-            MegIntermissionChat03 = true,
-            MegIntermissionChat04 = true,
-            MegIntermissionChat05 = true,
-            MegIntermissionChat06 = true,
+                },
+                -- Meg + Than scenes
+                [9] = {
+                    FunctionName = true
+                },
+                [10] = {
+                    FunctionName = true
+                },
+            }
         }
-    }
-})
+    })
+
+    -- Remove Megaera smooch statuses
+    ModUtil.Table.NilMerge( UnitSetData.NPCs, {
+        NPC_FurySister_01 = {
+            RepeatableTextLineSets = {
+                MegIntermissionChat01 = true,
+                MegIntermissionChat02 = true,
+                MegIntermissionChat03 = true,
+                MegIntermissionChat04 = true,
+                MegIntermissionChat05 = true,
+                MegIntermissionChat06 = true,
+            }
+        }
+    })
+end)
